297bad44f03d0a9687c2b9c0d41c76318ff90ecb
Adam Wong <adamwong246@gmail.com>
2014-02-17 10:20:52 -0800

    init commit

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function(grunt) {
+ 
+  // configure the tasks
+  grunt.initConfig({
+ 
+    copy: {
+      build: {
+        cwd: 'source',
+        src: [ '**', '!**/*.jade'  ],
+        dest: 'build',
+        expand: true
+      },
+    },
+    clean: {
+      build: {
+        src: [ 'build' ]
+      },
+    },
+    jade: {
+      compile: {
+        options: {
+          data: {}
+        },
+        files: [{
+          expand: true,
+          cwd: 'source',
+          src: [ '**/*.jade' ],
+          dest: 'build',
+          ext: '.html'
+        }]
+      }
+    },
+    watch: {
+      // stylesheets: {
+      //   files: 'source/**/*.styl',
+      //   tasks: [ 'stylesheets' ]
+      // },
+      // scripts: {
+      //   files: 'source/**/*.coffee',
+      //   tasks: [ 'scripts' ]
+      // },
+      jade: {
+        files: 'source/**/*.jade',
+        tasks: [ 'jade' ]
+      },
+      copy: {
+        files: [ 'source/**', '!source/**/*.styl', '!source/**/*.coffee', '!source/**/*.jade' ],
+        tasks: [ 'copy' ]
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 4000,
+          base: 'build',
+          hostname: '*'
+        }
+      }
+    }
+ 
+  });
+ 
+  // load the tasks
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+ 
+  // define the tasks
+  grunt.registerTask(
+    'build',
+    'Compiles all of the assets and copies the files to the build directory.',
+    [ 'clean:build', 'copy', 'jade']
+  );
+
+  grunt.registerTask(
+    'default',
+    'Watches the project for changes, automatically builds them and runs a server.',
+    [ 'build', 'connect', 'watch' ]
+  );
+
+};
