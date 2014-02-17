08bb2dec07f6747ab3a2db18cc1178b5544bcba5
Adam Wong <adamwong246@gmail.com>
2014-02-17 12:13:52 -0800

    stash

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,18 +57,37 @@ module.exports = function(grunt) {
       }
     },
     
-    // None of these names matter!
-    // Everything important is defined in frontmatter.
-    new_blog_entry: {
-      dest: "source/_blog/_:subject/",
-      filename: "_:subject" //relative to the dest
-    },
+    // // None of these names matter!
+    // // Everything important is defined in frontmatter.
+    // new_blog_entry: {
+    //   dest: "source/_blog/_:subject/",
+    //   filename: "_:subject" //relative to the dest
+    // },
 
     build_blog: {
       src: "source/_blog/*/",
       dest: "build/blog/:year/:month/:day/:title"
-    }
- 
+    },
+
+    prompt: {
+      new_blog_entry: {
+        options: {
+          questions: [
+            {
+              config: 'new_blog_entry.subject',
+              type: 'input',
+              message: 'What is the subject of this blog?',
+              // default: 'value',
+              // choices: 'Array|function(answers)',
+              // validate: function(value){},
+              // filter:  function(value){},
+              // when: function(answers){}
+            }
+          ]
+        }
+      },
+    },
+
   });
  
   // load the tasks
@@ -77,6 +96,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-prompt');
+
  
   // define the tasks
   grunt.registerTask(
@@ -91,12 +112,42 @@ module.exports = function(grunt) {
     [ 'build', 'connect', 'watch' ]
   );
 
+  grunt.registerTask('makeafile', 'A sample task that logs stuff.', function(name, contents){
+    var fs = require('fs');
+    fs.writeFile('name', 'contents', function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    });
+  });
+  
+  
+
+  // grunt.registerTask(
+  //   'make_a_file',
+    
+  //   function(name, contents){
+    //   var fs = require('fs');
+    //   fs.writeFile(name, contents, function(err) {
+    //       if(err) {
+    //           console.log(err);
+    //       } else {
+    //           console.log("The file was saved!");
+    //       }
+    //   });
+    // }
+  // );
+
   grunt.registerTask(
     'new_blog_entry',
     'Create a new blog entry.',
-    function(){
-      console.log("Hello World");
-    }
+    [
+      'prompt:new_blog_entry',
+      'make_a_file'
+
+    ]
   );
 
 };
